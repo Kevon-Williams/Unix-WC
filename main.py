8793cf58649c872cce8fb34ba4c44b417e937a61
Kevon Williams <KevonReuWilliams@gmail.com>
2024-04-24 09:16:22 -0400

    Add get_file_lines function and conditional to main

--- main.py
+++ main.py
@@ -7,20 +7,35 @@ def main():
 
     #command line argument with short and long form
     parser.add_argument('-c', '--check', type=str, help="Returns file size in bytes") 
+    parser.add_argument('-l', '--lines', type=str, help="Returns file size in bytes") 
 
     #parse command line arguments
     args = parser.parse_args() 
 
     #store entered input
     file = args.check 
+    file_lines = args.lines
 
-    print(f"{get_file_size(file)} bytes")  
+    if file != None:
+        print(f"{get_file_size(file)} bytes") 
+    elif file_lines != None:
+        print(f"{get_file_lines(file_lines)} lines") 
+
+    
 
 
 def get_file_size(file):
     with open(file, errors="ignore") as file:
         file.seek(0,os.SEEK_END)
         return file.tell()
+
+def get_file_lines(file):
+    line = 0
+    with open(file ,errors="ignore") as file:
+        for i in file:
+            line+=1
+    return line
+
 
 if __name__ == '__main__':
     main()
