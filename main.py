05439861425b56c831f63860bca8309fe3383736
Kevon Williams <KevonReuWilliams@gmail.com>
2024-04-23 21:20:22 -0400

    Fix size output

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="WIP 'wc' unix command line tool clone. ")
-    parser.add_argument('-c', '--count')
+    parser.add_argument('-c', '--check', type=str, help="Returns file size in bytes")
+    args = parser.parse_args()
 
-    file_name = input("Enter your file name: ")
-    print(get_file_size(file_name))
-    pass
+    file = args.check
+
+    print(get_file_size(file))
+
 
 def get_file_size(file):
     with open(file, errors="ignore") as file:
